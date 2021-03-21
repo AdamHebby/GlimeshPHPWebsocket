8082de526a73c3166220e3a5d99ae04077f5a278
AdamHebby <adamhebden56@gmail.com>
2021-03-21 21:10:34 +0000

    Fixed the issues I had

--- test.php
+++ test.php
@@ -2,9 +2,8 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$b = ['Authorization' => 'Bearer YOURTOKENHERE'];
 try {
-    \Ratchet\Client\connect('wss://glimesh.tv/api/socket', [], $b)->then(
+    \Ratchet\Client\connect('wss://glimesh.tv/api/socket/websocket?vsn=2.0.0&token=YOURTOKENHERE')->then(
         function($conn) {
             $conn->on('message', function($msg) {
                 echo "Received: {$msg}\n";
@@ -13,7 +12,7 @@ try {
             // https://glimesh.github.io/api-docs/docs/chat/websockets/
 
             $conn->send('["1","1","__absinthe__:control","phx_join",{}]');
-            $conn->send('["1","1","__absinthe__:control","doc",{"query":"subscription{ chatMessage(channelId: 6) { user { username avatar } message } }","variables":{} ]');
+            $conn->send('["1","1","__absinthe__:control","doc",{"query":"subscription{ chatMessage(channelId: 6) { user { username avatar } message } }","variables":{} } ]');
         }, function (\Throwable $e) {
             echo "Could not connect: {$e->getMessage()}\n";
         }
